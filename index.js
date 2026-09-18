4d3a969049baeeb3072b849430263b0a701afc44
Ali13698 <toopooli75@gmail.com>
2026-09-18 23:39:42 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+import { Bot, InlineKeyboard } from "grammy";
+
+const BOT_TOKEN = "8880600009:AAH3BnAlcdaHfBoHYybqtA8rRIndwg3_O6s";
+const WEBAPP_URL = "https://ali13698.github.io/ludo/";
+
+if (!BOT_TOKEN || BOT_TOKEN === "") {
+  console.error("BOT_TOKEN is not set!");
+  process.exit(1);
+}
+
+const bot = new Bot(BOT_TOKEN);
+
+bot.command("start", async (ctx) => {
+  const kb = new InlineKeyboard().webApp("🎲 بازی منچ", WEBAPP_URL);
+  await ctx.reply(
+    "🐋 سلام! به ربات آنجل خوش اومدی.\n\nبرای شروع بازی روی دکمه زیر بزن 👇",
+    { reply_markup: kb }
+  );
+});
+
+bot.command("help", async (ctx) => {
+  await ctx.reply("دستورات:\n/start - شروع\n/help - راهنما");
+});
+
+bot.catch((err) => {
+  console.error("Bot error:", err);
+});
+
+console.log("Starting bot...");
+bot.start({
+  onStart: (info) => console.log(`Bot @${info.username} started!`),
+});
