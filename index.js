ec1c9f1946f6ee773c88ed5759da63937f86809a
Ali13698 <toopooli75@gmail.com>
2026-09-20 05:44:05 +0330

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { DB } from "./db.js";
 import { Matchmaker } from "./matchmaking.js";
 import { BotPool } from "./bot-pool.js";
 
-const BOT_TOKEN = process.env.BOT_TOKEN || process.env.bot || "";
+const BOT_TOKEN = process.env.token || process.env.BOT_TOKEN || "";
 const WEBAPP_URL = "https://ali13698.github.io/ludo/";
 const PORT = process.env.PORT || 3000;
 
